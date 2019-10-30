d5359b9b968de9ce125f9bc4c7f7d3c35326d54a
Paker <carlosenumo@newm.com.br>
2019-10-30 02:46:09 -0300

    Funções de trim no util

--- cpp/src/util.cpp
+++ cpp/src/util.cpp
@@ -25,4 +25,21 @@ namespace util {
         }
         return parts;
     }
+
+    // trim from end of string (right)
+    std::string &rtrim(std::string &s, const char *t) {
+        s.erase(s.find_last_not_of(t) + 1);
+        return s;
+    }
+
+    // trim from beginning of string (left)
+    std::string &ltrim(std::string &s, const char *t) {
+        s.erase(0, s.find_first_not_of(t));
+        return s;
+    }
+
+    // trim from both ends of string (right then left)
+    std::string &trim(std::string &s, const char *t) {
+        return ltrim(rtrim(s, t), t);
+    }
 }
--- cpp/src/util.h
+++ cpp/src/util.h
@@ -15,6 +15,17 @@ namespace util {
     string join(const vector<string> &pieces, const char &glue);
 
     vector<string> split(const string &str, const char &delim);
+
+    static const char *whitespace = " \t\n\r\f\v";
+
+    // trim from end of string (right)
+    std::string &rtrim(std::string &s, const char *t = whitespace);
+
+    // trim from beginning of string (left)
+    std::string &ltrim(std::string &s, const char *t = whitespace);
+
+    // trim from both ends of string (right then left)
+    std::string &trim(std::string &s, const char *t = whitespace);
 }
 
 #endif //INSPECTOR_UTIL_H
