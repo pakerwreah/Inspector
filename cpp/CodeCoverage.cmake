02671d140b1944d6de7d6417681081eb436d0d0f
Carlos César Neves Enumo <paker_wreah@hotmail.com>
2020-09-06 07:29:01 -0300

    .

--- cpp/CodeCoverage.cmake
+++ cpp/CodeCoverage.cmake
@@ -1,14 +1,16 @@
 # Code Coverage Configuration
+add_library(coverage_config INTERFACE)
+
 option(CODE_COVERAGE "Enable coverage reporting" ON)
 
-if(CODE_COVERAGE)
+if (CODE_COVERAGE)
     # Add required flags (GCC & LLVM/Clang)
-    target_compile_options(inspector-cpp PRIVATE
-        -O0        # no optimization
-        -g         # generate debug info
-        --coverage # sets all required flags
-        -fprofile-instr-generate
-        -fcoverage-mapping
-    )
-    target_link_libraries(inspector-cpp -fprofile-instr-generate -fcoverage-mapping)
-endif(CODE_COVERAGE)
+    target_compile_options(coverage_config INTERFACE
+            -O0        # no optimization
+            -g         # generate debug info
+            --coverage # sets all required flags
+            -fprofile-instr-generate
+            -fcoverage-mapping
+            )
+    target_link_options(coverage_config INTERFACE -fprofile-instr-generate -fcoverage-mapping)
+endif (CODE_COVERAGE)
