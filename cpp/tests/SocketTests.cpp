6dab61816a3461e5818c867848d20875045171bc
Carlos César Neves Enumo <paker_wreah@hotmail.com>
2020-09-14 00:00:30 -0300

    Fix deadlocks

--- cpp/tests/SocketTests.cpp
+++ cpp/tests/SocketTests.cpp
@@ -27,29 +27,29 @@ TEST_CASE("Socket - Timeout") {
 
     SECTION("Success") {
         should_accept = true;
-        thread([&] {
+        thread th([&] {
             unique_ptr client = make_unique<Socket>();
             client->create();
             client->connect("localhost", port, {0, 1500});
             guard.unlock();
-        }).detach();
-
+        });
         guard.lock();
         REQUIRE(accepted_client != nullptr);
         CHECK(accepted_client->is_valid());
+        th.join();
     }
 
     SECTION("Fail") {
         should_accept = false;
-        thread([&] {
+        thread th([&] {
             unique_ptr client = make_unique<Socket>();
             client->create();
             client->connect("localhost", port, {0, 500});
             guard.unlock();
-        }).detach();
-
+        });
         guard.lock();
         CHECK(accepted_client == nullptr);
+        th.join();
     }
 }
 
@@ -61,8 +61,9 @@ TEST_CASE("SocketClient - IO") {
     REQUIRE(server.bind(port));
     REQUIRE(server.listen());
 
-    mutex read;
+    mutex read, check;
     read.lock();
+    check.lock();
     string response;
 
     thread th([&] {
@@ -72,20 +73,18 @@ TEST_CASE("SocketClient - IO") {
         SocketClient socketClient(move(client));
         read.lock(); // wait for server signal
         response = socketClient.read();
-        read.unlock(); // signal server to check for response
+        check.unlock(); // signal server to check for response
     });
 
     Socket *client = nullptr;
     CHECK(server.accept(client));
     CHECK(client != nullptr);
 
-    this_thread::sleep_for(1ms); // ensure thread lock
-
     SocketClient socketClient((unique_ptr<Socket>(client)));
     const string msg = "Test data";
-    read.unlock(); // signal thread to start reading
     CHECK(socketClient.send(msg));
-    read.lock(); // wait for response
+    read.unlock(); // signal thread to start reading
+    check.lock(); // wait for response
     CHECK(response == msg);
 
     th.join();
