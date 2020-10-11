ff9388ab8cc896e45eb9d9b446154447a6213164
Carlos César Neves Enumo <paker_wreah@hotmail.com>
2020-10-11 19:03:25 -0300

    addNetworkInterceptor won't intercept timeouts

--- android/app/app/src/main/java/br/newm/inspector_test/Http.java
+++ android/app/app/src/main/java/br/newm/inspector_test/Http.java
@@ -6,18 +6,24 @@ import br.newm.inspector.NetworkInterceptor;
 import okhttp3.Call;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.ResponseBody;
 
 public class Http {
     private static final OkHttpClient.Builder builder = new OkHttpClient.Builder()
-            .addNetworkInterceptor(new NetworkInterceptor());
+            // addNetworkInterceptor won't intercept timeouts
+            .addInterceptor(new NetworkInterceptor());
 
+    @SuppressWarnings("UnusedReturnValue")
     public static String request(String url) {
         OkHttpClient http = builder.build();
 
         Request.Builder req = new Request.Builder();
         Call call = http.newCall(req.url(url).build());
         try {
-            return call.execute().body().string();
+            ResponseBody body = call.execute().body();
+            if (body != null) {
+                return body.string();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
