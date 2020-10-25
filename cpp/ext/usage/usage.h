b575510d8b6ded423a193dabbc3e149c20b7bd2d
Carlos César Neves Enumo <paker_wreah@hotmail.com>
2020-10-25 01:22:36 -0300

    CPU/Memory usage plugin example

---
+++ cpp/ext/usage/usage.h
@@ -0,0 +1,15 @@
+//
+// Created by Paker on 24/10/20.
+//
+
+#ifndef INSPECTOR_USAGE_H
+#define INSPECTOR_USAGE_H
+
+#include "Inspector.h"
+
+struct Usage {
+    Usage(Inspector &inspector);
+};
+
+
+#endif //INSPECTOR_USAGE_H
